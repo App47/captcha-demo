17709ddc8164cecfce9480bd8e04ce6a0f5d7bd0
Chris Schroeder <chris@app47.com>
2025-06-05 07:40:56 -0400

    #2 add in the dns and tie it to the gateway.

--- infra/dns.tf
+++ infra/dns.tf
@@ -13,4 +13,15 @@ resource "aws_route53_record" "cdn_dns" {
   }
 }
 
+# Route 53 entry
+resource "aws_route53_record" "api_dns" {
+  zone_id = var.route53_zone_id
+  name    = "captcha-demo-api.app47.net"
+  type    = "A"
 
+   alias {
+    name = aws_apigatewayv2_domain_name.custom_api_domain.domain_name_configuration[0].target_domain_name
+    zone_id = aws_apigatewayv2_domain_name.custom_api_domain.domain_name_configuration[0].hosted_zone_id
+    evaluate_target_health = false
+  }
+}
