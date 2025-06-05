e2eef546b164f59a93987f9359f348e6df9469c8
Chris Schroeder <chris@app47.com>
2025-06-05 07:17:08 -0400

    #2 Add the submit handler along with the terraform actions.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # captcha-demo
 Hold the Captcha Demo for customers
+
+The purpose of this repo is show a working example of the App47 Captcha Service using a simple static web site:
+* [index.html](web/index.html)
+* [success.html](web/success.html)
+* [error.html](web/error.html)
+
+Along with a single lambda function that acts as the form handler for the static web site.
+* [submit.py](lambda/submit.py)
---
+++ infra/submit.tf
@@ -0,0 +1,22 @@
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_file = "${path.module}/../lambda/submit.py"
+  output_path = "${path.module}/lambda_payload.zip"
+}
+
+resource "aws_lambda_function" "captcha_handler" {
+  function_name = "captchaDemoFormHandler"
+  handler       = "submit.lambda_handler"
+  runtime       = "python3.12"
+  role          = aws_iam_role.lambda_exec.arn
+  filename      = data.archive_file.lambda_zip.output_path
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+}
+
+resource "aws_lambda_permission" "apigw_allow" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.captcha_handler.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.api.execution_arn}/*/*"
+}
