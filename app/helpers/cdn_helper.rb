6cb81017837ca86efdf57fcbf965972b90117f14
Chris Schroeder <chris@app47.com>
2025-09-02 06:45:47 -0400

    #31 PR feedback

--- app/helpers/cdn_helper.rb
+++ app/helpers/cdn_helper.rb
@@ -1,9 +1,8 @@
-
 module CdnHelper
   # @abstract Combine the CDN URL with the given path.
   # @param [String] path the path to load from the CDN.
   # @return [String]
   def cdn_url(path)
-    [Rails.application.credentials.cdn_url, path].join('/')
+    URI.join(Rails.application.credentials.cdn_url, path).to_s
   end
 end
